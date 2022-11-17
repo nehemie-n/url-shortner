6d7efacda06ab44e8a9754aa5695e389f1f41ea4
Nehemie <nehemie@opportuneur.co.uk>
2022-11-17 21:01:37 +0200

    #Feat: Added links resource

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  /**
+   * Add validation
+   */
+  app.useGlobalPipes(new ValidationPipe());
+  /**
+   * Add interceptors for some errors
+   */
+  await app.listen(process.env.TZ);
 }
 bootstrap();
